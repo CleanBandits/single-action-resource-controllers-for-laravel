717524bd72afb15ba0fd92f7241ade8584308513
juris.skesters <juris.skesters@di.lv>
2025-02-19 13:13:19 +0200

    Revert "Style fix"
    This reverts commit 311d8a1186fdfeda142dc16deb7b14036003dc48.

--- src/ide.php
+++ src/ide.php
@@ -4,24 +4,25 @@ namespace Illuminate\Routing {
     /**
      * @method void singleActionResources(array $resources, array $options = [])
      * @method \Illuminate\Routing\PendingResourceRegistration singleActionResource(string $name, ?string $controller = null, array $options = [])
-     *
      * @mixin \CleanBandits\SingleActionResourceControllers\Router
      */
-    class Router {}
+    class Router
+    {
+        //
+    }
 }
 
 namespace Illuminate\Support\Facades {
-    use Illuminate\Routing\Router;
-
     /**
      * @method static void singleActionResources(array $resources, array $options = [])
      * @method static \Illuminate\Routing\PendingResourceRegistration singleActionResource(string $name, ?string $controller = null, array $options = [])
-     *
      * @mixin \CleanBandits\SingleActionResourceControllers\Router
-     *
-     * @see Router
+     * @see \Illuminate\Routing\Router
      */
-    class Route extends Facade {}
+    class Route extends Facade
+    {
+        //
+    }
 }
 
 namespace CleanBandits\SingleActionResourceControllers {
@@ -29,10 +30,12 @@ namespace CleanBandits\SingleActionResourceControllers {
     use Illuminate\Routing\PendingResourceRegistration;
 
     /**
-     * @property Closure|PendingResourceRegistration $singleActionResource
-     *
+     * @property-read Closure|\Illuminate\Routing\PendingResourceRegistration $singleActionResource
      * @method PendingResourceRegistration singleActionResource(string $name, ?string $controller = null, array $options = [])
      * @method void singleActionResources(array $resources, array $options = [])
      */
-    class Router {}
+    class Router
+    {
+        //
+    }
 }
